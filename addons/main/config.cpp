89fc33fe68b1a6117ab029054987c9b842ff21d9
johnb432 <john.biselx@gmail.com>
2021-10-24 14:48:34 +0200

    Cleanup

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -13,6 +13,7 @@ class CfgPatches {
         };
         units[] = {};
         weapons[] = {};
+        VERSION_CONFIG;
     };
 };
 
@@ -21,15 +22,15 @@ class CfgMods {
         name = "TFAR - ACE Radio Setter - Extended";
         author = "johnb43";
         tooltipOwned = "TFAR - ACE Radio Setter - Extended";
-        picture = "\x\tfar_ace_extended\addons\main\ui\logo_ace_tfar_setter_extended.paa";
-        logoSmall = "\x\tfar_ace_extended\addons\main\ui\logo_ace_tfar_setter_extended.paa";
-        logo = "\x\tfar_ace_extended\addons\main\ui\logo_ace_tfar_setter_extended.paa";
-        logoOver = "\x\tfar_ace_extended\addons\main\ui\logo_ace_tfar_setter_extended.paa";
+        hideName = 0;
         hidePicture = 0;
         actionName = "Github";
         action = "https://github.com/johnb432/ACE-TFAR-Setter-Extended";
         description = "A simple quality of life mod for the Arma 3 TFAR Addon. Allows you to save multiple radio configurations and reload them at any point in the future.";
         overview = "A simple quality of life mod for the Arma 3 TFAR Addon. Allows you to save multiple radio configurations and reload them at any point in the future.";
+        overviewPicture = "\x\tfar_ace_extended\addons\main\ui\logo_ace_tfar_setter_extended.paa";
+        logo = "\x\tfar_ace_extended\addons\main\ui\logo_ace_tfar_setter_extended.paa";
+        picture = "\x\tfar_ace_extended\addons\main\ui\logo_ace_tfar_setter_extended.paa";
     };
 };
 
