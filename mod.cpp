d6cd511c60e32004fdff66d93cc7950d0abfd14f
johnb432 <john.biselx@gmail.com>
2021-10-26 14:14:42 +0200

    Update mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -14,12 +14,12 @@ action = "https://github.com/johnb432/ACE-TFAR-Setter-Extended";
 description = "A simple quality of life mod for the Arma 3 TFAR Addon. Allows you to save multiple radio configurations and reload them at any point in the future.";
 // Overview text, displayed from the extension menu
 overview = "A simple quality of life mod for the Arma 3 TFAR Addon. Allows you to save multiple radio configurations and reload them at any point in the future.";
+// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too; https://community.bistudio.com/wiki/ImageToPAA
+picture = "\x\tfar_ace_extended\addons\main\ui\logo_ace_tfar_setter_extended.paa";
 // Logo displayed in the main menu
 logo = "\x\tfar_ace_extended\addons\main\ui\logo_ace_tfar_setter_extended.paa";
 // When the mouse is over, in the main menu
 // logoOver = ;
 // Display next to the item added by the mod
 // logoSmall = ;
-// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too; https://community.bistudio.com/wiki/ImageToPAA
-picture = "\x\tfar_ace_extended\addons\main\ui\logo_ace_tfar_setter_extended.paa";
 overviewPicture = "\x\tfar_ace_extended\addons\main\ui\logo_ace_tfar_setter_extended.paa";
